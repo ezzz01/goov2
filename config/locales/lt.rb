747067c08d64254afc76f4b3a0627479d90c6929
ezhux <ezhux@ezz.(none)>
2010-05-16 18:59:10 +0200

    butiniausi failai

---
+++ config/locales/lt.rb
@@ -0,0 +1,313 @@
+# Sample localization file for Lithuanian. Add more files in this directory for other locales.
+# See http://github.com/svenfuchs/rails-i18n/tree/master/rails%2Flocale for starting points.
+{
+:lt => {
+  :mydate => "Data",
+  :start_page => "Pirmas puslapis",
+  :users => "Vartotojai",
+  :wiki => "Wiki",
+  :community => "Bendruomenė",
+  :blogs => "Dienoraščiai",
+  :help => "Pagalba",
+  :about => "Apie projektą",
+  :project_blog => "Projekto blogas",
+  :countries => "Šalys",
+  :title => "Pavadinimas",
+  :tag_list => "Žymės",
+  :tags => "Žymės",
+  :blog => "Tinklaraštis",
+  :rss_source => "RSS šaltinio adresas svetainėje",
+  :goout_blog => "Go-out tinklaraštis",
+  :external_blog => "Išorinis tinklaraštis",
+  :studies => "Studijos",
+  :zeitgeist => "Zeitgeist",
+  :entry => "Įrašas",
+  :body => "Body?",
+  :new_entry => "Sukurti naują",
+  :ask_question => "Užduok klausimą",
+  :create_new_country => "Sukurti naują šalį",
+  :new_university => "Sukurti naują universitetą ar organizaciją",
+  :new_study_program => "Sukurti naują studijų programą",
+  :new_exchange_program => "Sukurti naują mainų programą",
+  :new_activity_area => "Sukurti naują veiklos sritį",
+
+  :back => "Atgal",
+  :show => "Rodyti",
+  :answer => "Atsakyti",
+  :all_answers => "Visi atsakymai",
+  :new_answer => "Naujas atsakymas",
+  :first_3_answers => "Paskutiniai atsakymai:",
+  :vote_for_answer => "Balsuoti už atsakymą. Paspaudus dar kartą balsas bus atšauktas",
+  :vote_against_answer => "Balsuoti prieš atsakymą. Paspaudus dar kartą balsas bus atšauktas",
+  :revoke_vote => "Paspaudus dar kartą balsas bus atšauktas",
+  :log_in_to_vote => "Prisijunkite, jei norite balsuoti",
+
+  #activity types
+  :full_study => "Pilnos studijos",
+  :fullstudy => "Pilnos studijos",
+  :exchange_study => "Mainų programa",
+  :exchangestudy => "Mainų programa",
+  :internship => "Stažuotė / Praktika",
+
+  :activity_type => "Veikla",
+  :exchange_program_name => "Mainų programos pavadinimas", 
+  :organization_name => "Organizacija",
+  :country => "Šalis",
+  :subject_area => "Studijų sritis",
+  :study_program => "Studijų programa",
+  :from => "Nuo",
+  :to => "Iki",
+  :activity_area => "Veiklos sritis",
+
+
+  #countries
+  :new_country => "Nauja šalis",
+  #page titles
+  :user_profile => "Vartotojo {{username}} profilis",
+  :edit_user => "Redaguoti profilį",
+  :edit_basic_info => "Redaguoti vartotojo profilį",
+  :goout_zeitgeist => "Go-out.lt zeitgeist",
+  #register, login, user profile
+  :register => "Registracija",
+  :do_register => "Užsiregistruoti",
+  :login => "Prisijungti",
+  :logout => "Atsijungti",
+  :no_such_user => "Tokio vartotojo nėra!",
+  :register_button => "Registruotis",
+  :login_button => "Prisijungti",
+  :enter_your_details => "Įveskite savo duomenis",
+  :username => "Vartotojo vardas", 
+  :email => "El. paštas",
+  :email_hint => "El. pašto adresas kitiems vartotojams nėra rodomas",
+  :blog_url => "Tinklaraščio adresas",
+  :blog_title => "Tinklaraščio pavadinimas",
+  :password => "Slaptažodis",
+  :password_confirm => "Slaptažodis (dar kartą)",
+  :avatar => "Avataras",
+  :gender => "Lytis",
+  :name => "Vardas",
+  :surname => "Pavardė",
+  :name_surname => "Vardas, pavardė",
+  :he => "Jis",
+  :she => "Ji",
+  :birthdate => "Gimimo data",
+  :remember_me => "Prisiminti duomenis?",
+  :update_email => "Atnaujinti el. pašto adresą",
+  :update_blog_url => "Atnaujinti tinklaraščio adresą",
+
+  :send => "Siųsti",
+  :delete => "Ištrinti",
+  :destroy => "Ištrinti",
+  :update => "Atnaujinti",
+  :edit => "Redaguoti",
+  :cancel => "Atšaukti",
+  :create => "Sukurti",
+  :close => "Uždaryti",
+  :start_updating_feeds => "Pradėti RSS atnaujinimą",
+  :stop_updating_feeds => "Sustabdyti RSS atnaujinimą",
+  #flashes:
+  :post_created_successfully => "Įrašas sėkmingai sukurtas",
+  :post_updated_successfully => "Įrašas sėkmingai atnaujintas",
+  :question_created_successfully => "Klausimas įrašytas",
+  :no_permission => "Jums nesuteiktos teisės atlikti šį veiksmą",
+  :registration_successful => "Užsiregistravote sėkmingai",
+  :login_successful => "Prisijungėte sėkmingai",
+  :logout_successful => "Atsijungėte sėkmingai",
+  :invalid_username_password => "Neteisingas vartotojo vardas ir/arba slaptažodis",
+  :attribute_updated => "atnaujinta(s)",
+  :user => "Vartotojas",
+  :logged_in => "prisijungė",
+  :created => "sukurtas",
+  :are_you_sure => "Ar tikrai ištrinti?",
+  :user_was_successfully_updated => "Vartotojo duomenys atnaujinti sėkmingai",
+
+  #notices:
+  :university_is_pending => "Kol svetainės administratorius nepatvirtins naujai sukurto įrašo, jį matysite tik Jūs.<br />Platesnį universiteto aprašymą galėsite sukurti Universitetų sąraše",
+  :organization_is_pending => "Kol svetainės administratorius nepatvirtins naujai sukurto įrašo, jį matysite tik Jūs.<br />Platesnį organizacijos aprašymą galėsite sukurti Organizacijų sąraše",
+  :study_program_is_pending => "Kol svetainės administratorius nepatvirtins naujai sukurto įrašo, jį matysite tik Jūs.<br />Platesnį studijos programos aprašymą galėsite sukurti studijų programų sąraše",
+  :exchange_program_is_pending => "Kol svetainės administratorius nepatvirtins naujai sukurto įrašo, jį matysite tik Jūs.<br />Platesnį mainų programos aprašymą galėsite sukurti mainų programų sąraše",
+  :activity_area_is_pending => "Kol svetainės administratorius nepatvirtins naujai sukurto įrašo, jį matysite tik Jūs.<br />Platesnį veiklos srities aprašymą galėsite sukurti veiklos sričių sąraše",
+  :country_is_pending => "Kol svetainės administratorius nepatvirtins naujai sukurto įrašo, jį matysite tik Jūs.<br />Platesnį šalies aprašymą galėsite sukurti veiklos sričių sąraše",
+
+  #tooltips:
+  :will_be_deleted_on_system => "Dėmesio, įrašas bus ištrintas iš go-out.lt, bet ne iš pirminio šaltinio!",
+  :will_be_edited_on_system => "Dėmesio, redaguoti galite tik go-out.lt saugomą įrašo kopiją, ne originalą!",
+
+  #log:
+  :last_wiki_updates => "Paskutiniai wiki pataisymai",
+  :added_new_concept => "{{user}} pridėjo naują puslapį \"{{title}}\"",
+  :deleted_new_concept => "{{user}} ištrynė puslapį \"{{title}}\"",
+  :edited_concept => "{{user}} redagavo puslapį \"{{title}}\"",
+
+  #blog
+  :blog_posts => "Vartotojo {{username}} tinklaraščio įrašai", 
+  :add_new_post => "Sukurti naują įrašą",
+  :edit_post => "Redaguoti įrašą '{{title}}'",
+  :new_post => "Naujas įrašas", 
+  :new_post_comment => "Komentuoti įrašą",
+  :new_comment_reply => "Atsakyti į komentarą",
+  :comments => "Komentarų",
+  #comments
+  :anonymous => "Anonimas",
+  #friends
+  :add_as_friend => "Įtraukti į draugų sąrašą",
+  :added_friend => "Vartotojas įtrauktas į draugų sąrašą",
+  :remove_friend => "Pašalinti iš draugų sąrašo",
+  :removed_friend => "Vartotojas pašalintas iš draugų sąrašo",
+  :friends => "Draugai",
+  :friend_of => "Šių vartotojų draugas",
+  :unable_to_add_friend => "Klaida pridedant draugą",
+
+  #questions
+  :full_question => "Pilnas klausimas",
+  :all_questions => "Visi klausimai",
+  :view_answers => "Peržiūrėti atsakymus",
+  :number_of_answers => "Atsakymų",
+
+  #wiki
+  :wiki_start_page => "Pirmas wiki puslapis",
+  :categories => "Kategorijos",
+  :edit_concept => "Redaguoti puslapį",
+  :all_pages_in_category => "Visi puslapiai kategorijoje",
+  :wanted_pages_in_this_category => "Trūkstami puslapiai šioje kategorijoje",
+  :pages_without_references => "Puslapiai, į kuriuos nėra nuorodų iš kitų puslapių",
+  :link_from => "nuoroda iš",
+  :pages_without_references_in_category => "Puslapiai kategorijoje \"{{category}}\", į kuriuos nėra nuorodų iš kitų puslapių",
+  :want_to_contribute => "Nori prisidėti? Šitų puslapių trūksta!",
+  :contribute_hint => "Spausk ant klaustuko po pavadinimu ir rašyk!",
+  :author => "Autorius",
+  :content => "Turinys",
+  :new_page => "Naujas wiki puslapis",
+  :create_new_page => "Sukurti naują wiki puslapį",
+  :page_was_successfully_created => "Naujas puslapis sukurtas sėkmingai",
+  :page_was_successfully_updated => "Puslapis atnaujintas sėkmingai",
+  :last_edit => "Paskutinis redagavimas", 
+  :edit_history => "Redagavimo istorija",
+  :no_such_word => "Tokio žodžio nėra",
+  :for_page => "puslapiui",
+  :new_revision => "Nauja revizija",
+  #errors
+  :error_on_saving => "Klaida išsaugant",
+  :error_saving_university => "Klaida išsaugant. Gal toks universitetas jau yra?",
+  :error_saving_organization => "Klaida išsaugant. Gal tokia organizacija jau yra?",
+  :error_saving_exchange_program => "Klaida išsaugant. Gal tokia mainų programa jau yra?",
+  :error_saving_activity_area => "Klaida išsaugant. Gal tokia veiklos sritis jau yra?",
+  :error_saving_study_program => "Klaida išsaugant. Gal tokia studijų programa jau yra?",
+  #user
+  :select_country => "Pasirinkite šalį",
+  :select_university => "Pasirinkite universitetą",
+  :select_study_type => "Pasirinkite studijų tipą",
+  :select_subject_area => "Pasirinkite studijų sritį",
+  :select_study_program => "Pasirinkite studijų programą",
+  :select_exchange_program => "Pasirinkite mainų programą",
+  :select_activity_area => "Pasirinkite veiklos sritį",
+
+  :select_country_first => "Pirmiausia pasirinkite šalį",
+  :select_subject_area_first => "Pirmiausia pasirinkite studijų sritį",
+
+  :current => "Iki dabar",
+
+  #organization_types
+  :university => "Universitetas",
+  :company => "Įmonė",
+  :ngo => "Nevyriausybinė organizacija",
+  #custom_errors
+  :username_error => "turi susidėti tik iš raidžių, skaičių, brūkšių (-) ir pabraukimo brūkšnių (-)",
+  :no_spaces => "neturi turėti tarpų",
+  :not_correct => "neteisingas",
+  :must_be_valid => "turi būti teisingas",
+  :could_not_connect => "Nepavyko gauti įrašų iš išorinio tinklaraščio. Patikrinkite adresą. <br>Rodomi tik sistemoje išsaugoti įrašai.",
+
+
+  :models => {
+      :user => {
+        :attributes => {
+          :username => "Vartotojo vardas",
+          :password => "Slaptažodis",
+          :email => "El. pašto adresas",
+          :blog_url => "Tinklaraščio adresas",
+          :avatar => "Avataras",
+          }
+        } ,
+        :concept => {
+        :attributes => {
+            :title => "Pavadinimas",
+        }
+     }
+
+     },
+
+  :activerecord => {
+    :attributes => {
+        :user => {  
+          :username => "Vartotojo vardas",
+          :password => "Slaptažodis",
+          :email => "El. paštas",
+          :blog_url => "Tinklaraščio adresas",
+        },
+        :concept => {
+        :attributes => {
+            :title => "Pavadinimas",
+        }
+     }
+
+    },
+    :errors => {
+      :template => {
+        :header => {
+          :one => "Įrašas nebuvo išsaugotas dėl 1 klaidos",
+          :other => "Dėl {{count}} klaidų įrašas nebuvo išsaugotas"
+        },
+        :body => "problemų kilo su šiais laukais:" 
+     },
+      :messages => {
+        :accepted => "must be accepted",
+        :blank => "negali būti tuščia", 
+        :confirmation => "neatitinka patvirtinimo",
+        :empty => "negali būti tuščia", 
+        :equal_to => "turi būti lygu {{count}}",
+        :even => "turi būti lyginis", 
+        :exclusion => "yra rezervuota(s)",
+        :greater_than => "turi būti daugiau už {{count}}", 
+        :greater_than_or_equal_to => "turi būti daugiau arba lygu {{count}}",
+        :inclusion => "nėra sąraše",
+        :invalid => "yra neteisinga(s)", 
+        :less_than => "turi būti mažiau už {{count}}",
+        :less_than_or_equal_to => "turi būti mažiau arba lygu {{count}}",
+        :record_invalid => "Negaliojantis įrašas", 
+        :not_a_number => "turi būti skaičius",
+        :odd => "turi būti lyginis",
+        :taken => "jau yra sistemoje",
+        :too_long => "per ilgas (maksimalus ilgis yra {{count}} simbolių)",
+        :too_short => "per trumpas (minimalus ilgis yra {{count}} simboliai)",
+        :wrong_length => "neteisingo ilgio (turi būti {{count}} simboliai)"
+      }
+  }
+  },
+
+
+  :time => {
+    :formats => {
+      :default => "%Y-%m-%d",
+      :short => "%m-%d", 
+      :long => "%Y-%m-%d %H:%M",
+    },
+    :am => "am", 
+    :pm => "pm" 
+   }, 
+  
+  :date => {
+    :formats => {
+      :default => "%Y-%m-%d",
+      :short => "%b %d",
+    },
+    :day_names => [ "Pirmadienis", "Antradienis", "Trečiadienis", "Ketvirtadienis", "Penktadienis", "Šeštadienis", "Sekmadienis" ],
+    :abbr_day_names => ["Pirm.", "Antr.", "Treč.", "Ketv.", "Penkt.", "Šešt.", "Sekm."],
+    # Don't forget the nil at the beginning; there's no such thing as a 0th month
+    :month_names => ["~", "Sausio", "Vasario", "Kovo", "Balandžio", "Gegužės", "Birželio", "Liepos", "Rugpjūčio", "Rugsėjo", "Spalio", "Lapkričio", "Gruodžio"],
+    :abbr_month_names => ["~", "Saus.", "Vas.", "Kov.", "Bal.", "Geg.", "Birž.", "Liep.", "Rugpj.", "Rugs.", "Spal.", "Lapk.", "Gruod."],
+    # Used in date_select and datime_select.
+    :order => [ :year, :month, :day ]
+  }
+}
+} 
