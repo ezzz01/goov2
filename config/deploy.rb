e56991f1fb11db0993cf0cb423ecc33d23deb3d6
ezhux <ezhux001@yahoo.com>
2010-09-06 21:36:42 +0200

    freeze rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,61 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+default_run_options[:pty] = true
+set :application, "goout"
+set :repository,  "git@github.com:ezhux/goov2.git"
+set :deploy_to, "/home/ezhux/goout"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :domain, "go-out.lt"
+set :scm, :git
+set :scm_verbose, true
+set :branch, 'master'
+set :use_sudo, false
+set :ssh_options, { :forward_agent => true }
+set :keep_releases, 3
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user, "ezhux"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+#set :deploy_via, :remote_cache
+set :deploy_via, :copy
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+role :db,  domain 
+
+before "deploy:restart", "environment:production"
+before "deploy:start", "environment:production"
+before "deploy:restart", "db:symlink" 
+before "deploy:migrate", "db:symlink" 
+after "deploy:symlink", "customs:symlink"
+after "deploy", "deploy:cleanup"
+
+namespace :environment do
+  task :production do
+    set :rails_env, 'production' 
+  end
+end
+
+namespace(:customs) do
+ task :symlink, :roles => :app do
+    run <<-CMD
+      ln -nfs #{shared_path}/avatars #{release_path}/public/avatars
+    CMD
+  end
+end
+
+namespace :deploy do
+   task :start do 
+     run "#{try_sudo} touch #{File.join(current_path,'log','feed_updater.log')}"
+     run "#{try_sudo} touch #{File.join(current_path,'log','wikilog.log')}"
+   end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
+
+namespace :db do
+  desc "Make symlink for database yaml" 
+  task :symlink do
+    run "ln -nfs ~/.ssh/database.yml #{release_path}/config/database.yml"
+  end
+end
